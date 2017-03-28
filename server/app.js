08f8e75d27f3bc502379cde7d6b24803369f1aea
step1041 <steve.wiggins7@gmail.com>
2017-03-27 21:30:17 -0400

    fix npm start

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import Sequelize from 'sequelize';
 import routes from './routes';
 
-mongoose.connect('mongodb://localhost:27017/redditclone', () =>{
-    console.log("connected to mongodb");
+const app = express();
+
+var sequelize = new Sequelize('athena_development', 'root', 'led107a100', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
 
-const app = express();
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
 //middleware
 app.use(bodyParser.json());
 
--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,7 @@ import express from 'express';
 
 //controller imports
 import basicController from './controllers/basicController';
-import userController from './controllers/userController';
-import postController from './controllers/postController';
-import commentController from './controllers/commentController';
+import gameController from './controllers/gameController';
 
 
 
@@ -12,14 +10,5 @@ const routes = express();
 //Basic Routes
 routes.get('/', basicController.get);
 
-//User Routes
-routes.post('/signup', userController.post);
-
-//Post Routes
-routes.post('/post', postController.post);
-routes.get('/posts', postController.getAll);
-
-//Comment Routes
-routes.post('/comment', commentController.post);
 
 export default routes; 
