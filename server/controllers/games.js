ed15d0a19258383507053a35870043fe0f4cddab
Stephen Wiggins <steve.wiggins7@gmail.com>
2017-03-29 04:38:15 -0400

    add create

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,21 +1,28 @@
 const Game = require('../models').Game;
 
 
-//BOILERPLATE CODE FROM ANOTHER PROJECT - TO BE CHANGED
-
 module.exports= {
     create(req, res) {
-        return Todo
-        .create({
-            title: req.body.title,
-        })
-        .then((todo) => res.status(201).send(todo))
-        .catch((error) => res.status(400).send(error));
+        return Game
+            .create({
+                game_integer: req.body.game_integer,
+                sr_change: req.body.sr_change,
+                rank: req.body.rank,
+                streak: req.body.streak,
+                map: req.body.map,
+                score: req.body.score,
+                group_size: req.body.group_size,
+                heroes: req.body.heroes,
+                notes: req.body.notes,
+
+            })
+            .then((games) => res.status(201).send(games))
+            .catch((error) => res.status(400).send(error));
     },
     findAll(req, res) {
-    return Game
-        .findAll()
-        .then(games => res.status(200).send(games))
-        .catch(error => res.status(400).send(error));
+        return Game
+            .findAll()
+            .then(games => res.status(200).send(games))
+            .catch(error => res.status(400).send(error));
     }
 };
