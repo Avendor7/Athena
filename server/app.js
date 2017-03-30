d842f0c48c4365d6045ead897ceb282907ba0276
step1041 <steve.wiggins7@gmail.com>
2017-03-29 21:40:54 -0400

    fixes for pull request

--- server/app.js
+++ server/app.js
@@ -1,28 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Sequelize from 'sequelize';
 import routes from './routes';
 
 const app = express();
-
-var sequelize = new Sequelize('athena_development', 'root', 'led107a100', {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
-sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
 
 //middleware
 app.use(bodyParser.json());
--- server/controllers/games.js
+++ server/controllers/games.js
@@ -2,31 +2,31 @@ const Game = require('../models').Game;
 
 
 module.exports= {
-    create(req, res) {
-        return Game
-            .create({
-                game_number: req.body.game_number,
-                sr_change: req.body.sr_change,
-                rank: req.body.rank,
-                streak: req.body.streak,
-                map: req.body.map,
-                score: req.body.score,
-                group_size: req.body.group_size,
-                heroes: req.body.heroes,
-                notes: req.body.notes
-            })
-            .then((game) => res.status(201).send(game))
-            .catch((error) => res.status(400).send(error));
-    },
+  create(req, res) {
+    return Game
+        .create({
+            game_number: req.body.game_number,
+            sr_change: req.body.sr_change,
+            rank: req.body.rank,
+            streak: req.body.streak,
+            map: req.body.map,
+            score: req.body.score,
+            group_size: req.body.group_size,
+            heroes: req.body.heroes,
+            notes: req.body.notes
+        })
+        .then((game) => res.status(201).send(game))
+        .catch((error) => res.status(400).send(error));
+  },
 
-    findAll(req, res) {
-        return Game
-            .findAll()
-            .then(games => res.status(200).send(games))
-            .catch(error => res.status(400).send(error));
-    },
+  findAll(req, res) {
+    return Game
+        .findAll()
+        .then(games => res.status(200).send(games))
+        .catch(error => res.status(400).send(error));
+  },
 
-    update(req, res) {
+  update(req, res) {
     return Game
       .findById(req.params.gameId)
       .then(game => {
