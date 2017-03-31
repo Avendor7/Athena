292acaaca13b887ca02b1763e6805852281b7438
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-03-31 18:04:24 -0400

    install eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
+  }
+};
