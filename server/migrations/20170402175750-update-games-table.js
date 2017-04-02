c9ecf1639b5f4e98fece21909e97515bc89789fc
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-04-02 14:58:35 -0400

    run migrations in a transaction

--- server/migrations/20170402175750-update-games-table.js
+++ server/migrations/20170402175750-update-games-table.js
@@ -1,31 +1,36 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => Promise.all([
-    // Remove unneeded columns
-    queryInterface.removeColumn('Games', 'game_number'),
-    queryInterface.removeColumn('Games', 'sr_change'),
-    queryInterface.removeColumn('Games', 'streak'),
-    queryInterface.removeColumn('Games', 'map'),
-    queryInterface.removeColumn('Games', 'score'),
-    queryInterface.removeColumn('Games', 'group_size'),
-    queryInterface.removeColumn('Games', 'heroes'),
-    queryInterface.removeColumn('Games', 'notes'),
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+    (transaction) => Promise.all([
+      // Remove unneeded columns
+      queryInterface.removeColumn('Games', 'game_number', {transaction}),
+      queryInterface.removeColumn('Games', 'sr_change', {transaction}),
+      queryInterface.removeColumn('Games', 'streak', {transaction}),
+      queryInterface.removeColumn('Games', 'map', {transaction}),
+      queryInterface.removeColumn('Games', 'score', {transaction}),
+      queryInterface.removeColumn('Games', 'group_size', {transaction}),
+      queryInterface.removeColumn('Games', 'heroes', {transaction}),
+      queryInterface.removeColumn('Games', 'notes', {transaction}),
 
-    //add missing columns
-    queryInterface.addColumn('Games', 'outcome', Sequelize.TEXT),
-  ]),
+      //add missing columns
+      queryInterface.addColumn('Games', 'outcome', Sequelize.TEXT, {transaction}),
+    ])
+  ),
 
-  down: (queryInterface, Sequelize) => Promise.all([
-    queryInterface.addColumn('Games', 'game_number', Sequelize.INTEGER),
-    queryInterface.addColumn('Games', 'sr_change', Sequelize.INTEGER),
-    queryInterface.addColumn('Games', 'streak', Sequelize.INTEGER),
-    queryInterface.addColumn('Games', 'map', Sequelize.TEXT),
-    queryInterface.addColumn('Games', 'score', Sequelize.STRING),
-    queryInterface.addColumn('Games', 'group_size', Sequelize.INTEGER),
-    queryInterface.addColumn('Games', 'heroes', Sequelize.TEXT),
-    queryInterface.addColumn('Games', 'notes', Sequelize.TEXT),
+  down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+    (transaction) => Promise.all([
+      queryInterface.addColumn('Games', 'game_number', Sequelize.INTEGER, {transaction}),
+      queryInterface.addColumn('Games', 'sr_change', Sequelize.INTEGER, {transaction}),
+      queryInterface.addColumn('Games', 'streak', Sequelize.INTEGER, {transaction}),
+      queryInterface.addColumn('Games', 'map', Sequelize.TEXT, {transaction}),
+      queryInterface.addColumn('Games', 'score', Sequelize.STRING, {transaction}),
+      queryInterface.addColumn('Games', 'group_size', Sequelize.INTEGER, {transaction}),
+      queryInterface.addColumn('Games', 'heroes', Sequelize.TEXT, {transaction}),
+      queryInterface.addColumn('Games', 'notes', Sequelize.TEXT, {transaction}),
 
-    queryInterface.removeColumn('Games', 'outcome'),
-  ]),
+      //remove added columns
+      queryInterface.removeColumn('Games', 'outcome', {transaction}),
+    ])
+  ),
 };
