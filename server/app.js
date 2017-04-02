7dc67c77c22a70bd9e7dbe04e735c0560fc9c591
step1041 <steve.wiggins7@gmail.com>
2017-04-02 14:01:49 -0400

    fix CORS

--- server/app.js
+++ server/app.js
@@ -9,4 +9,10 @@ app.use(bodyParser.json());
 
 app.use('/api', routes);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 module.exports = app;
