79ad825e484acac3c823b8b92589290523f6eca5
Avendor7 <steve.wiggins7@gmail.com>
2017-04-06 21:03:11 -0400

    add game model attributes

--- client/app/models/game.js
+++ client/app/models/game.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    rank: DS.attr('number'),
+    outcome: DS.attr('string'),
+    createdAt: DS.attr('date'),
+    updatedAt: DS.attr('date')
 });
