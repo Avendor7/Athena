3c24fd228277dc56dc6f808431462e6d6d4edbb9
Avendor7 <steve.wiggins7@gmail.com>
2017-04-22 00:03:19 -0400

    get data from submit button

--- client/app/components/add-game.js
+++ client/app/components/add-game.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        saveGame() {
+            const outcome = this.get('outcome');
+            const rank = this.get('rank');
+            console.log(outcome);
+            console.log(rank);
+        }
+  }
+
 });
