3ede5296b8781edb210eb68fa2b9b9c90194e104
Avendor7 <steve.wiggins7@gmail.com>
2017-04-27 18:01:56 -0400

    saves a new record, but missing data

--- client/app/components/add-game.js
+++ client/app/components/add-game.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         saveGame() {
-            const outcome = this.get('model.outcome');
-            const rank = this.get('model.rank');
+            const outcome = this.get('outcome');
+            const rank = this.get('rank');
             this.saveGameInParent(outcome, rank);
         }
-  }
+    }
 });
--- client/app/controllers/application.js
+++ client/app/controllers/application.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    
+   model() {
+        return Ember.RSVP.hash({
+            games: this.store.findAll('game')
+        });
+    },
+
+    actions:{
+        saveGameInParent(outcome, rank) {
+
+            var store = this.store;
+            console.log("outcome: " + outcome);
+            console.log("rank: " + rank);
+
+            var saveGame = store.createRecord('game', {
+                'rank': rank,
+                'outcome': outcome
+            });
+            saveGame.save();
+            console.log("record created");
+        }
+    }
 });
