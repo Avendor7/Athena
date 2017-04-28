e02d669f0d15b94e075e67f18d4b7d0011db75d1
Avendor7 <steve.wiggins7@gmail.com>
2017-04-28 01:50:14 -0400

    add editGame code

--- client/app/components/game-table.js
+++ client/app/components/game-table.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions:{
+        editGame(){
+            const outcome = this.get('outcome');
+            const rank = this.get('rank');
+            this.saveUpdatedGameInParent(outcome, rank);
+        }
+    }
 });
