26eb38bc430178fe0a7e5a2d9fa9dfcad71a6a41
Avendor7 <steve.wiggins7@gmail.com>
2017-05-30 01:00:51 -0400

    add edit and save buttons that change table context

--- client/app/components/game-table.js
+++ client/app/components/game-table.js
@@ -1,11 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    editing: false,
     actions:{
         editGame(){
+            this.set('editing', false);
             const outcome = this.get('outcome');
             const rank = this.get('rank');
-            this.saveUpdatedGameInParent(outcome, rank);
+            //this.saveUpdatedGameInParent(outcome, rank);
+        },
+        isEditing(){
+            this.set('editing', true);
+            console.log(this.editing);
         }
     }
+    //TODO: fix edit toggle to be per-entry and pre-populate
 });
