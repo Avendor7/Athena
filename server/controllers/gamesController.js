285df4b30bfaab624339810889ccf7345c92d687
Avendor7 <steve.wiggins7@gmail.com>
2017-06-08 17:48:37 -0400

    beautify all the things

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -10,21 +10,21 @@ module.exports = {
    * @param {*} res res.status
    */
   create(req, res) {
-        return Game
-        .create({
-            rank: req.body.game.rank,
-            outcome: req.body.game.outcome,
-        })
-        .then((game) => res.status(201).send({
-            status: 201,
-            message: 'Game created',
-            game: game,
-        }))
-        .catch((error) => res.status(500).send({
-            status: 500,
-            message: 'Internal server error',
-            errors: [error],
-        }));
+    return Game
+      .create({
+        rank: req.body.game.rank,
+        outcome: req.body.game.outcome,
+      })
+      .then((game) => res.status(201).send({
+        status: 201,
+        message: 'Game created',
+        game: game,
+      }))
+      .catch((error) => res.status(500).send({
+        status: 500,
+        message: 'Internal server error',
+        errors: [error],
+      }));
   },
 
   /**
@@ -38,18 +38,18 @@ module.exports = {
    */
   findAll(req, res) {
     return Game
-        .findAll()
-        .then(games => res.status(200).send({
-            status: 200,
-            message: 'Games found',
-            games: games,
-        }))
-        .catch(error => res.status(500).send({
-            status: 500,
-            message: 'Internal server error',
-            errors: [error],
-        }));
-    },
+      .findAll()
+      .then(games => res.status(200).send({
+        status: 200,
+        message: 'Games found',
+        games: games,
+      }))
+      .catch(error => res.status(500).send({
+        status: 500,
+        message: 'Internal server error',
+        errors: [error],
+      }));
+  },
 
   /**
    * Returns one entry from the server
@@ -61,32 +61,33 @@ module.exports = {
    */
   findOne(req, res) {
     return Game
-        .findById(req.params.gameId)
-        .then(games =>  {
-            if (games){
-            res.status(200).send({
-                status: 200,
-                message: 'Games found',
-                games: games,
-            })}else{
-            throw {
-                status: 404,
-                message: 'Game not found',
-                errors: ['Game not found'],
-            };
-        }})
-        .catch((error) => {
-            if (error.status) {
-            res.status(error.status).send(error);
-            }
-            else {
-            res.status(500).send({
-                status: 500,
-                message: 'Internal server error',
-                errors: [error],
-            });
-            }
-        });
+      .findById(req.params.gameId)
+      .then(games => {
+        if (games) {
+          res.status(200).send({
+            status: 200,
+            message: 'Games found',
+            games: games,
+          })
+        } else {
+          throw {
+            status: 404,
+            message: 'Game not found',
+            errors: ['Game not found'],
+          };
+        }
+      })
+      .catch((error) => {
+        if (error.status) {
+          res.status(error.status).send(error);
+        } else {
+          res.status(500).send({
+            status: 500,
+            message: 'Internal server error',
+            errors: [error],
+          });
+        }
+      });
   },
 
   /**
@@ -99,36 +100,35 @@ module.exports = {
    */
   update(req, res) {
     return Game
-        .findById(req.params.gameId)
-        .then(game => {
-            if (!game) {
-                throw {
-                    status: 404,
-                    message: 'Game not found',
-                    errors: ['Game not found'],
-                };
-            }
-            return game
-            .update({
-                rank: req.body.game.rank || game.rank,
-                outcome: req.body.game.outcome || game.outcome,
-            });
-        })
-        .then((game) => res.status(200).send({
-            status: 200,
-            message: 'Game updated',
-            game: game,
-        }))
-        .catch((error) => {
-            if (error.status) {
-                res.status(error.status).send(error);
-            }
-            else {
-            res.status(500).send({
-                status: 500,
-                message: 'Internal server error',
-                errors: [error],
-            });
+      .findById(req.params.gameId)
+      .then(game => {
+        if (!game) {
+          throw {
+            status: 404,
+            message: 'Game not found',
+            errors: ['Game not found'],
+          };
+        }
+        return game
+          .update({
+            rank: req.body.game.rank || game.rank,
+            outcome: req.body.game.outcome || game.outcome,
+          });
+      })
+      .then((game) => res.status(200).send({
+        status: 200,
+        message: 'Game updated',
+        game: game,
+      }))
+      .catch((error) => {
+        if (error.status) {
+          res.status(error.status).send(error);
+        } else {
+          res.status(500).send({
+            status: 500,
+            message: 'Internal server error',
+            errors: [error],
+          });
         }
       });
   },
@@ -143,36 +143,35 @@ module.exports = {
    */
   destroy(req, res) {
     return Game
-        .findById(req.params.gameId)
-        .then(game => {
-            if (!game) {
-                throw {
-                    status: 404,
-                    message: 'Game not found',
-                    errors: ['Game not found'],
-                };
-            }
-
-            return game
-        })
-        .then((game) => {
-            res.status(200).send({
-            status: 200,
-            message: 'Game destroyed',
-            game: game,
-            });
-        })
-        .catch((error) => {
-            if (error.status) {
-                res.status(error.status).send(error);
-            }
-            else {
-                res.status(500).send({
-                    status: 500,
-                    message: 'Internal server error',
-                    errors: [error],
-                });
-            }
-        });
+      .findById(req.params.gameId)
+      .then(game => {
+        if (!game) {
+          throw {
+            status: 404,
+            message: 'Game not found',
+            errors: ['Game not found'],
+          };
         }
+
+        return game
+      })
+      .then((game) => {
+        res.status(200).send({
+          status: 200,
+          message: 'Game destroyed',
+          game: game,
+        });
+      })
+      .catch((error) => {
+        if (error.status) {
+          res.status(error.status).send(error);
+        } else {
+          res.status(500).send({
+            status: 500,
+            message: 'Internal server error',
+            errors: [error],
+          });
+        }
+      });
+  }
 };
