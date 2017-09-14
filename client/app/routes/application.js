406b77055ff3773f4d7097c473a1ac2f75b8aa3a
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-09-13 20:05:16 -0400

    move games to route and page

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        return Ember.RSVP.hash({
-            games: this.store.findAll('game')
-        });
-    },
-    actions: {
-
-   }
 });
