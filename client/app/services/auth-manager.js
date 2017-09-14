6fb05f1bb268bc1288d23a67c575b22ba85a0060
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-09-13 22:38:03 -0400

    pass auth code from Blizzard to server

--- client/app/services/auth-manager.js
+++ client/app/services/auth-manager.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import ENV from 'client/config/environment';
 
 export default Ember.Service.extend({
   authenticate(type, token) {
-    console.log(type, token)
+    Ember.$.post(`${ENV.APP.serverUrl}/api/auth`, {type, token})
+      .then((response) => console.log(response));
   }
 });
