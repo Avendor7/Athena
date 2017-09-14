97e93aefb736a83e0b872d163c1608196f2477bc
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-09-13 22:34:38 -0400

    add request logging to the server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const expressLogging = require('express-logging');
+
 const routes = require('./routes');
 
 const app = express();
 
 //middleware
 app.use(bodyParser.json());
+app.use(expressLogging(console));
 
 //enable cors on ALL routes
 app.use(cors());
