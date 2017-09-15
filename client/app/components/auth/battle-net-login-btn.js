153bbec85c21880800fc13c96d51eea7507c136f
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-09-14 22:37:30 -0400

    add logic to the battle.net button

--- client/app/components/auth/battle-net-login-btn.js
+++ client/app/components/auth/battle-net-login-btn.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
+import ENV from 'client/config/environment'
 
 export default Ember.Component.extend({
   classNames: ['auth-btn', 'auth-battle-net'],
   actions: {
-    login() {
-      console.log('foobar');
+    login: function () {
+      let query = {
+        client_id: ENV.APP.battleNetKey,
+        redirect_uri: window.location.protocol + '//' + window.location.host + '/auth/battlenet',
+        response_type: 'code'
+      };
+
+      let queryStr = Object.entries(query)
+        .map((pair) => `${pair[0]}=${encodeURIComponent(pair[1])}`)
+        .join('&');
+
+      window.location = `https://${ENV.APP.battleNetRegion.toLowerCase()}.battle.net/oauth/authorize?${queryStr}`;
     }
   }
 });
