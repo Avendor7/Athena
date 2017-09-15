01648eb97b1070c4792334e24b2e341a3285a867
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-09-14 22:19:29 -0400

    allow server to be hosted on https

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
+
+const ENV = require('./config/environment');
 const app = require('./app');
 
-app.listen(3000, () =>{
-  console.info('running on port 3000');
+let server = null;
+
+if (ENV.https) {
+  const https = require('https');
+  let key = fs.readFileSync('./server.snakeoil.key', 'utf8');
+  let cert = fs.readFileSync('./server.snakeoil.cert', 'utf8');
+  server = https.createServer({key, cert}, app);
+}
+else {
+  const http = require('http');
+  server = http.createServer(app);
+}
+
+
+server.listen(ENV.port, () =>{
+  let hostname = `${ENV.https ? 'https' : 'http'}://${ENV.hostname}:${ENV.port}`;
+  console.info(`listening on ${hostname}`);
+  console.info('ENV:', ENV.env);
 });
 
